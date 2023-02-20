2329d47057b30516660772ebb39434f154151ab7
artiecannv <thefiftharthur@gmail.com>
2023-02-20 16:24:47 -0700

    seeded and created the database

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,28 +1,28 @@
 INSERT INTO department (id, name)
-VALUES (001, "Sales Lead"),
-       (002, "Legal Team Lead"),
-       (003, "Lead Engineer"),
-       (004, "Account Manager")
+VALUES (001, "Sales"),
+       (002, "Legal"),
+       (003, "Engineering"),
+       (004, "Accounting");
 
        
 INSERT INTO role (id, title, salary, department_id)
-VALUES (001, "Sales Lead", 100000, "Sales"),
-       (002, "Salesperson", 80000, "Sales"),
-       (003, "Lead Engineer", 150000, "Engineering"),
-       (004, "Software Engineer", 120000, "Engineering"),
-       (005, "Account Manager", 160000, "Finance"),
-       (006, "Accountant", 125000, "Finance"),
-       (007, "Legal Team Lead", 250000, "Legal"),
-       (008, "Lawyer", 190000, "Legal"),
+VALUES (001, "Sales Lead", 100000, 001),
+       (002, "Salesperson", 80000, 001),
+       (003, "Lead Engineer", 150000, 002),
+       (004, "Software Engineer", 120000, 002),
+       (005, "Account Manager", 160000, 003),
+       (006, "Accountant", 125000, 003),
+       (007, "Legal Team Lead", 250000, 004),
+       (008, "Lawyer", 190000, 004);
 
 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (001, "John", "Doe", "Sales Lead", null),
-       (002, "Mike", "Chan", "Salesperson", "John Doe"),
-       (003, "Ashley", "Rodriguez", "Lead Engineer", null),
-       (004, "Kevin", "Tupik", "Software Engineer", "Ashley Rodriguez"),
-       (005, "Kunal", "Singh", "Account Manager", null),
-       (006, "Malia", "Brown", "Accountant", "Kunal Singh"),
-       (007, "Sarah", "Lourd", "Legal Team Lead", null),
-       (008, "Tom", "Allen", "Lawyer", "Sarah Lourd");
+VALUES (001, "John", "Doe", 001, null),
+       (002, "Mike", "Chan", 002, 001),
+       (003, "Ashley", "Rodriguez", 003, null),
+       (004, "Kevin", "Tupik", 004, 003),
+       (005, "Kunal", "Singh", 005, null),
+       (006, "Malia", "Brown", 006, 005),
+       (007, "Sarah", "Lourd", 007, null),
+       (008, "Tom", "Allen", 008, 007);
 
