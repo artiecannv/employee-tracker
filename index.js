43251daf38e876690b5ba43c53ee394b809a5b2c
artiecannv <thefiftharthur@gmail.com>
2023-03-02 15:33:08 -0700

    added the add employee prompt

--- index.js
+++ index.js
@@ -2,20 +2,17 @@ const mysql = require("mysql2");
 const inquirer = require("inquirer");
 const cTable = require("console.table");
 require("dotenv").config();
-const db = mysql.createConnection(
-  {
-    host: "localhost",
-    port: 3306,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-  }
-);
+const db = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
 
 // db.connect(function(error) {
 //   if (error) throw error
 // });
-
 
 const startMenu = [
   {
@@ -33,36 +30,78 @@ const startMenu = [
       "Quit",
     ],
   },
-  // {
-  //     type: "",
-  //     message: "",
-  //     name: "",
-  // }
+];
+
+const addEmployee = [
+  {
+    type: "input",
+    message: "What is the employee's first name?",
+    name: "addEmployee1",
+  },
+  {
+    type: "input",
+    message: "What is the employee's last name?",
+    name: "addEmployee2",
+  },
+  {
+    type: "input",
+    message: "What is the employee's role?",
+    name: "addEmployee3",
+  },
+  {
+    type: "input",
+    message: "What is the employee's manager?",
+    name: "addEmployee4",
+  },
 ];
 
 function menuPrompts() {
-    inquirer.prompt(startMenu)
-    .then((answers) => {
-      switch (answers.initialChoice) {
-        case "View All Employees":
-          viewEmployees()
-          break;
-      
-        default:
-          break;
-      }
-    })
-    // exit = answers.initialChoice === "Quit" ? true : false;
+  inquirer.prompt(startMenu).then((answers) => {
+    switch (answers.initialChoice) {
+      case "View All Employees":
+        viewEmployees();
+        break;
+      case "Add Employee":
+        addEmployee();
+        break;
+
+      default:
+        console.log("Have a good day!");
+        process.exit();
+    }
+  });
 }
 
 const viewEmployees = () => {
-  db.query("SELECT employee.id, CONCAT(employee.first_name,' ',employee.last_name) AS full_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name,' ',manager.last_name) AS manager FROM employee LEFT OUTER JOIN employee manager ON employee.manager_id = manager.id JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id", (err, data) => {
-    if (err) {
-      throw err
-    } else {
-      console.table (data)
+  db.query(
+    "SELECT employee.id, CONCAT(employee.first_name,' ',employee.last_name) AS full_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name,' ',manager.last_name) AS manager FROM employee LEFT OUTER JOIN employee manager ON employee.manager_id = manager.id JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id",
+    (err, data) => {
+      if (err) {
+        throw err;
+      } else {
+        console.table(data);
+      }
+      menuPrompts();
     }
-  })
-}
+  );
+};
+
+const addEmployeePrompt = () => {
+  inquirer.prompt(addEmployee).then((answers) => {
+    db.query(
+      "INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?,?,?,?)",
+      [
+        answers.addEmployee1,
+        answers.addEmployee2,
+        answers.addEmployee3,
+        answers.addEmployee4,
+      ],
+      (err, res) => {
+        if (err) throw err;
+        console.log("Successfully created Employee")
+      }
+    );
+  });
+};
 
 menuPrompts();
